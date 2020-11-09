08a3a65f4930849eed3100cb51f27cb512289eb5
aday <aday.05.12.96@gmail.com>
2020-11-09 13:02:40 +0600

    added filter ui

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:fit/components/active-workouts.dart';
 import 'package:fit/components/workouts-list.dart';
 import 'package:fit/services/auth.dart';
@@ -10,44 +11,41 @@ class HomePage extends StatefulWidget {
   _HomaPageState createState() => _HomaPageState();
 }
 
-
 class _HomaPageState extends State<HomePage> {
   int sectionIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    var  navigationBar = CurvedNavigationBar(
+      items: <Widget>[
+        Icon(Icons.fitness_center),
+        Icon(Icons.search),
+      ],
+      index: sectionIndex,
+      height: 50,
+      onTap: (int index) => setState(() => sectionIndex = index),
+    );
     return Container(
       child: Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        appBar: AppBar(
-          title: Text('Fitness'),
-          leading: Icon(Icons.fitness_center),
-          actions: [
-            FlatButton.icon(
-              onPressed: () {
-                AuthService().logout();
-              },
-              icon: Icon(
-                Icons.exit_to_app,
-                color: Colors.red,
-              ),
-              label: SizedBox.shrink(),
-            )
-          ],
-        ),
-        body: sectionIndex == 0 ? WorkoutsList():ActiveWorkouts(),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.fitness_center), label: 'My Workouts'),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Find Worouts'),
-          ],
-          unselectedItemColor: Colors.black26,
-          backgroundColor: Colors.white,
-          currentIndex: sectionIndex,
-          onTap: (int index){
-            setState(() {sectionIndex = index;});
-          },
-        ),
+          backgroundColor: Theme.of(context).primaryColor,
+          appBar: AppBar(
+            title: Text(sectionIndex == 0 ? 'Workout' : "Search Workout"),
+            leading: Icon(Icons.fitness_center),
+            actions: [
+              FlatButton.icon(
+                onPressed: () {
+                  AuthService().logout();
+                },
+                icon: Icon(
+                  Icons.exit_to_app,
+                  color: Colors.red,
+                ),
+                label: SizedBox.shrink(),
+              )
+            ],
+          ),
+          body: sectionIndex == 0 ? WorkoutsList() : ActiveWorkouts(),
+          bottomNavigationBar: navigationBar
       ),
     );
   }
